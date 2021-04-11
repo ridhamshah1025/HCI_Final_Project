b2b40b27f9c544f5baadfe7929cca2622a77aeb3
Ridham <ridhamshah1025@gmail.com>
2021-04-12 01:06:51 +0530

    added exit button in finish activity

--- app/src/main/java/com/example/hci_final_project/Finish.java
+++ app/src/main/java/com/example/hci_final_project/Finish.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Finish extends AppCompatActivity {
 
     TextView textView33;
+    Button button45;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,13 @@ public class Finish extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_finish);
 
+        button45 = findViewById(R.id.button45);
+        button45.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
         textView33 = findViewById(R.id.textView33);
         Intent intent = getIntent();
         String str = intent.getStringExtra("fileLocation");
