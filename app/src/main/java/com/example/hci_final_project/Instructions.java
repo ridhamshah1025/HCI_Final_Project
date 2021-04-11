64c1a582782b036fc140bb61571b4564d3fbf6a5
Ridham <ridhamshah1025@gmail.com>
2021-04-11 16:25:05 +0530

    Add Button in instruction activity

--- app/src/main/java/com/example/hci_final_project/Instructions.java
+++ app/src/main/java/com/example/hci_final_project/Instructions.java
@@ -1,14 +1,40 @@
 package com.example.hci_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Instructions extends AppCompatActivity {
-
+Button button44;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+
+        button44 = findViewById(R.id.button44);
+        button44.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(Instructions.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                        || ActivityCompat.checkSelfPermission(Instructions.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    // this will request for permission when user has not granted permission for the app
+                    ActivityCompat.requestPermissions(Instructions.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
+                    if (ActivityCompat.checkSelfPermission(Instructions.this, Manifest.permission.READ_EXTERNAL_STORAGE) == -1) {
+                        Toast.makeText(Instructions.this, "You must grant storage access.", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Intent intent = new Intent(Instructions.this, StartScreen.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
