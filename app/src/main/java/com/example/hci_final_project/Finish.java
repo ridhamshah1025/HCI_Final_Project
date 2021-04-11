e262aabeea813cbfdd622f6e8305823b7a020839
Ridham <ridhamshah1025@gmail.com>
2021-04-12 00:50:47 +0530

    Added title bar in finish activity

--- app/src/main/java/com/example/hci_final_project/Finish.java
+++ app/src/main/java/com/example/hci_final_project/Finish.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.TextView;
 
 public class Finish extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class Finish extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_finish);
 
         textView33 = findViewById(R.id.textView33);
